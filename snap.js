42af09668b774c274e71088993a37d095181beb8
Dominic Whittle <dom@weekends.ws>
2022-08-20 17:00:10 +1000

    baseContext and custom contexts added

--- snap.js
+++ snap.js
@@ -27,19 +27,38 @@ function create_snapshots(profile_path, out_dir) {
     for (const url of profile.urls) {
       let file_prefix = file_prefix_from_url(url)
 
-      for (const device_name of profile.devices) {
-        console.log("📸", url, device_name)
-        const device_suffix = device_name.replace(/ /g, "-")
-        const file_name = file_prefix
-          ? file_prefix + "--" + device_suffix
-          : "home" + "--" + device_suffix
+      // Merge defined contexts with Playwright included contexts from device names
+      const contexts = profile.contexts || []
+      for (const device_name of profile.devices || []) {
+        contexts.push({
+          name: device_name,
+          ...devices[device_name]
+        })
+      }
 
-        const context = await browser.newContext({ ...devices[device_name] })
-        const page = await context.newPage()
+      for (let ctx of contexts) {
+        console.log("📸", url, ctx.name)
+
+        // Mix in base context rules
+        if (profile.baseContext) {
+          ctx = Object.assign({}, profile.baseContext, ctx)
+        }
+
+        const file_suffix = ctx.name.replace(/ /g, "-")
+        const file_name = file_prefix
+          ? file_prefix + "--" + file_suffix
+          : "home" + "--" + file_suffix
+
+        const browserContext = await browser.newContext(ctx)
+        const page = await browserContext.newPage()
+
         for (const script of profile.initScripts || []) {
           await page.addInitScript(script)
           // The script is evaluated after the document was created but before any of its scripts were run. This is useful to amend the JavaScript environment, e.g. to seed Math.random.
         }
+
+        // #todo await browserContext.addCookies([cookieObject1, cookieObject2]);
+
         await page.goto(url)
         await page.screenshot({
           path: `${out_dir}/${file_name}.png`,
